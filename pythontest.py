627e4ac8f4512e50e164b6b5b5d33e0c3ae02e78
Barry Rowlingson <b.rowlingson@lancs.ac.uk>
2019-04-17 21:47:49 +0100

    try reqs

--- pythontest.py
+++ pythontest.py
@@ -1,2 +1,15 @@
 
 print "This must be python 2"
+from inform import display, warn, error
+display(
+    'Display is like print'
+    'except that it supports logging and can be disabled.'
+    ,sep=', ')
+
+# Display is like print, except that it supports logging and can be disabled.
+
+warn('warnings get a header that is printed in yellow.')
+# warning: warnings get a header that is printed in yellow.
+
+error('errors get a header that is printed in red.')
+# error: errors get a header that is printed in red.
